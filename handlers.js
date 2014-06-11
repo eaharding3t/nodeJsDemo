bc24d448987414148bee20de680bfbe83cec3733
kae2k <kae2k@virginia.edu>
2014-06-11 10:14:24 -0400

    added try/catch for aws

--- handlers.js
+++ handlers.js
@@ -64,8 +64,7 @@ function courseExpand(getData, response)
 function sectionExpand(getData, response)
 {
 	//Generates the details table based on the type of database selected
-	if(getData['databaseType'] == 0)
-	{
+	if(getData['databaseType'] == 0) {
 		var locate = 0;
 		var rows = [];
 		rows[0] = {subject:'math',courseID:'algebra',section:'001',time:130,room:'001',
@@ -84,10 +83,8 @@ function sectionExpand(getData, response)
 			'room': '305','waitList':20,'slotsTotal':60,'slotsOpen':12,'teacher': 'computerEngineeringTeacher'};
 		rows[7]={'subject':'electrical','courseID':'wiring','section':'436','time':235,
 				'room': '325','waitList':10,'slotsTotal':60,'slotsOpen':12,'teacher': 'electricalTeacher'};
-		for(var i = 0; i < rows.length; i++)
-		{
-			if(rows[i]['subject'] == getData['subject'] && rows[i]['courseID'] == getData['courseID'] && rows[i]['section'] == getData['section'])
-			{
+		for(var i = 0; i < rows.length; i++) {
+			if(rows[i]['subject'] == getData['subject'] && rows[i]['courseID'] == getData['courseID'] && rows[i]['section'] == getData['section']) {
 				locate = i;
 			}
 		}
@@ -121,7 +118,7 @@ function sectionExpand(getData, response)
 	}
 	if (getData['databaseType'] == 1) {
 		var connection = mysql.createConnection({
-				host : "db.2020ar.com",
+				host : "pocdb.2020ar.com",
 						database: "mydb",
 						user: "testuser",
 						password: "password123"
@@ -163,61 +160,62 @@ function sectionExpand(getData, response)
 	 		response.end();
 	 	});
 	}
-	else if (getData['databaseType'] == 2){
-		AWS.config.loadFromPath('./config.json');
-		var db = new AWS.DynamoDB();
-		var table ="";
-		var params = {
-			TableName:'course',
-			IndexName: "section"+"-index",
-			KeyConditions: {
-				"section":
-				{
-					"AttributeValueList": [{"S" : getData['section']}],
-					ComparisonOperator: 'EQ'
+	else if (getData['databaseType'] == 2) {
+		try {
+			AWS.config.loadFromPath('/var/www/html/repo/nodeJsDemo/config.json');
+			var db = new AWS.DynamoDB();
+			var table ="";
+			var params = {
+				TableName:'course',
+				IndexName: "section"+"-index",
+				KeyConditions: {
+					"section":
+					{
+						"AttributeValueList": [{"S" : getData['section']}],
+						ComparisonOperator: 'EQ'
+					}
 				}
 			}
-		}
-		try{
-		db.query(params, function(err,data){
-			var locate = 0;
-			for(var i =0;i<data['Items'].length;i++)
-			{
-				if(getData['course']==data['Items'][i]['course']['S'] && getData['subject']==data['Items'][i]['subject']['S'])
-				{
-					locate = i;
-					break;
-				}
-			}
-			//console.log(data['Items']);
-			table += '<table border="1" style="width:500px">'+
-						'<tr>'+
-  							'<td>Slots open</td>'+
-  							'<td>Slots total</td>'+		
-  							'<td>Teacher</td>'+
-  							'<td>Time</td>'+
-  							'<td>Room</td>'+
-  							'<td>Waitlist</td>'+
-						'</tr>'+
-						'<tr>'+
-							'<td>' + String(data['Items'][locate]['slotsOpen']['N']) + '</td>'+
-							'<td>' + String(data['Items'][locate]["slotsTotal"]['N']) + '</td>'+
-							'<td>' + String(data['Items'][locate]["teacher"]['S']) + '</td>'+
-							'<td>' + String(data['Items'][locate]["time"]['S']) + '</td>'+
-							'<td>' + String(data['Items'][locate]["room"]['S']) + '</td>'+
-							'<td>' + String(data['Items'][locate]["waitList"]['N']) + '</td>'+
-		 				'</tr>'+
-						'</table>';
+			try {
+				db.query(params, function(err,data) {
+					var locate = 0;
+					for (var i=0; i<data['Items'].length; i++) {
+						if (getData['course']==data['Items'][i]['course']['S'] && getData['subject']==data['Items'][i]['subject']['S']) {
+							locate = i;
+							break;
+						}
+					}
+					//console.log(data['Items']);
+					table += '<table border="1" style="width:500px">'+
+							'<tr>'+
+  								'<td>Slots open</td>'+
+  								'<td>Slots total</td>'+		
+  								'<td>Teacher</td>'+
+  								'<td>Time</td>'+
+  								'<td>Room</td>'+
+  								'<td>Waitlist</td>'+
+							'</tr>'+
+							'<tr>'+
+								'<td>' + String(data['Items'][locate]['slotsOpen']['N']) + '</td>'+
+								'<td>' + String(data['Items'][locate]["slotsTotal"]['N']) + '</td>'+
+								'<td>' + String(data['Items'][locate]["teacher"]['S']) + '</td>'+
+								'<td>' + String(data['Items'][locate]["time"]['S']) + '</td>'+
+								'<td>' + String(data['Items'][locate]["room"]['S']) + '</td>'+
+								'<td>' + String(data['Items'][locate]["waitList"]['N']) + '</td>'+
+		 					'</tr>'+
+							'</table>';
 
-			var headers = {};
-  			headers["Content-Type"] = "text/html";
-    		headers["Access-Control-Allow-Origin"] = "*";
-    		var temp = "document.getElementById('subjectL').value, document.getElementById('courseL').value, document.getElementById('subjectL')";
-    		table+='<br><div id = "fileUpload"><button onclick = "fileUpload('+temp+')">Upload a file</button></div>';
-			response.writeHead(200, headers);
-			response.write(table);
-			response.end();
-		});
+					var headers = {};
+  					headers["Content-Type"] = "text/html";
+    				headers["Access-Control-Allow-Origin"] = "*";
+    				var temp = "document.getElementById('subjectL').value, document.getElementById('courseL').value, document.getElementById('subjectL')";
+    				table +='<br><div id = "fileUpload"><button onclick = "fileUpload('+temp+')">Upload a file</button></div>';
+					response.writeHead(200, headers);
+					response.write(table);
+					response.end();
+				});
+			}
+			catch(err){}
 		}
 		catch(err){}
 	}
@@ -263,153 +261,153 @@ function sectionExpand(getData, response)
 //called when user presses "Upload a file" button
 //nested in order to run synchronously
 function fileUpload(getData, response) {
-	AWS.config.loadFromPath('./config.json');
-	var s3 = new AWS.S3();
-	var file = "./courseDetails.txt";
-	var details = "subject=" + getData['subject'] + " courseID="+getData['courseID']+" section="+getData['section'];
-	var bucket = 'ke_bucket';
-	var S3_Info = '';
+	try {
+		AWS.config.loadFromPath('/var/www/html/repo/nodeJsDemo/config.json');
+		var s3 = new AWS.S3();
+		var file = "./courseDetails.txt";
+		var details = "subject=" + getData['subject'] + " courseID="+getData['courseID']+" section="+getData['section'];
+		var bucket = 'ke_bucket';
+		var S3_Info = '';
 
-	var fileCounter;
-	var params = {
-  		Bucket: bucket,
-	};
-	//get correct number of file to upload
-	s3.listObjects(params, function(err, data) {
-  		if (err) console.log(err, err.stack);
-  		else     fileCounter = data.Contents.length;	
-		fileCounter++;
-		var currentFile = "courseDetails" + String(fileCounter) + ".txt";
-		//write file to server
-		fs.writeFile(file, details,function(err){
-			if(err){throw err;}
-			var file2 = "./courseDetails.txt";
-			//read file again and put the data into S3 params
-			fs.readFile(file2, function(err, data) {
-				if(err) {
-					throw err;
-				}
-				var params = {
-  					Bucket: bucket,
-  					Key: currentFile,
-  					Body: data,
-				}
-				//Add object to S3 with the given params
-				s3.putObject(params, function(err, data) {
-  					if (err) console.log(err, err.stack);
-  					//beginning building html
-  					S3_Info += '<h1>S3 Information</h1>' +
-					'<p>File: ' + currentFile + ' put into Bucket: ' + bucket + '</p>';
-					S3_Info += '<p>Bucket: ' + bucket + ' currently contains ';			      
-  					var params = {
-      				Bucket: bucket,
-  					};
-  					//list objects in bucket, add to html
-  					s3.listObjects(params, function(err, data) {
-    					if (err) console.log(err, err.stack);
-    					else {
-      						for (var i=0; i<data.Contents.length; i++) {
-        						S3_Info += String(data.Contents[i].Key) + ', ';
-      						}   
-    					}
-    					//get access control policy on current bucket, add to html
-    					S3_Info += '</p><p>Bucket: ' + bucket + ' is owned by ';
-    					s3.getBucketAcl(params, function(err, data) {
-      						if (err) console.log(err, err.stack);
-      						else {
-        						S3_Info += data.Owner.DisplayName + ' with permissions: ' + data.Grants[0].Permission + '</p>';
-        					}
-      						var headers = {};
-  							headers["Content-Type"] = "text/html";
-  							headers["Access-Control-Allow-Origin"] = "*";
-							response.writeHead(200, headers);
-							response.write(S3_Info);
-							response.end();
-    					});
-  					});	           
+		var fileCounter;
+		var params = {
+  			Bucket: bucket,
+		};
+		//get correct number of file to upload
+		s3.listObjects(params, function(err, data) {
+  			if (err) console.log(err, err.stack);
+  			else     fileCounter = data.Contents.length;	
+			fileCounter++;
+			var currentFile = "courseDetails" + String(fileCounter) + ".txt";
+			//write file to server
+			fs.writeFile(file, details,function(err){
+				if(err){throw err;}
+				var file2 = "./courseDetails.txt";
+				//read file again and put the data into S3 params
+				fs.readFile(file2, function(err, data) {
+					if(err) {throw err;}
+					var params = {
+  						Bucket: bucket,
+  						Key: currentFile,
+  						Body: data,
+					}
+					//Add object to S3 with the given params
+					s3.putObject(params, function(err, data) {
+  						if (err) console.log(err, err.stack);
+  						//beginning building html
+  						S3_Info += '<h1>S3 Information</h1>' +
+						'<p>File: ' + currentFile + ' put into Bucket: ' + bucket + '</p>';
+						S3_Info += '<p>Bucket: ' + bucket + ' currently contains ';			      
+  						var params = {
+      						Bucket: bucket,
+  						};
+  						//list objects in bucket, add to html
+  						s3.listObjects(params, function(err, data) {
+    						if (err) console.log(err, err.stack);
+    						else {
+      							for (var i=0; i<data.Contents.length; i++) {
+        							S3_Info += String(data.Contents[i].Key) + ', ';
+      							}   
+    						}
+    						//get access control policy on current bucket, add to html
+    						S3_Info += '</p><p>Bucket: ' + bucket + ' is owned by ';
+    						s3.getBucketAcl(params, function(err, data) {
+      							if (err) console.log(err, err.stack);
+      							else {
+        							S3_Info += data.Owner.DisplayName + ' with permissions: ' + data.Grants[0].Permission + '</p>';
+        						}
+      							var headers = {};
+  								headers["Content-Type"] = "text/html";
+  								headers["Access-Control-Allow-Origin"] = "*";
+								response.writeHead(200, headers);
+								response.write(S3_Info);
+								response.end();
+    						});
+  						});	           
+					});
 				});
 			});
 		});
-	});	
+	}
+	catch(err) {}	
 }
 
 function loadTest(getData, response) {
-	if(getData['loadTest'] == 'true')
-		{
-			switch(getData['databaseType'])
-			{
-				case '1':
-					var connection = mysql.createConnection({
-						host : "db.2020ar.com",
-						database: "mydb",
-						user: "testuser",
-						password: "password123"
-					});
-					var rand = String(Math.round(Math.random()*100000));
-					connection.query("select * from Subjects, Courses where Courses.SubjectID = Subjects.ID OR Courses.ID ="+rand+" LIMIT "+rand,function(error,rows,feilds){
-						var loadTestList = "<p>"+rand+"<br/>";
-						for (var i = 0; i < rows.length; i++){
-							loadTestList += String(rows[i]['Name'])+'<br/>';
-						}
-						loadTestList += "</p>";
-						var headers = {};
-  						headers["Content-Type"] = "text/html";
-  						headers["Access-Control-Allow-Origin"] = "*";
-						response.writeHead(200, headers);
-						response.write(loadTestList);
-						response.end();
-					});
-					break;
-				case '2':
+	if(getData['loadTest'] == 'true') {
+		switch(getData['databaseType'])	{
+			case '1':
+				var connection = mysql.createConnection({
+					host : "pocdb.2020ar.com",
+					database: "mydb",
+					user: "testuser",
+					password: "password123"
+				});
+				var rand = String(Math.round(Math.random()*100000));
+				connection.query("select * from Subjects, Courses where Courses.SubjectID = Subjects.ID OR Courses.ID =" + rand + " LIMIT "+ rand, function(error,rows,feilds) {
+					var loadTestList = "<p>"+rand+"<br/>";
+					for (var i = 0; i < rows.length; i++){
+						loadTestList += String(rows[i]['Name'])+'<br/>';
+					}
+					loadTestList += "</p>";
+					var headers = {};
+  					headers["Content-Type"] = "text/html";
+  					headers["Access-Control-Allow-Origin"] = "*";
+					response.writeHead(200, headers);
+					response.write(loadTestList);
+					response.end();
+				});
+				break;
+			case '2':
+				try {
 					AWS.config.loadFromPath('/var/www/html/repo/nodeJsDemo/config.json');
 					var db = new AWS.DynamoDB();
 					var rand = Math.round(Math.random()*10000);
 					var params = {
 						TableName: 'course',
 						AttributesToGet: ['sectionID']
-					//	Limit: rand
+						//	Limit: rand
 					};
-					try{
-					db.scan(params, function(err, data){
-						var loadTestList = "<p>"+rand+"<br/>";
-						if(data != null){
-							for(var i = 0; i < data['Items'].length; i++)
-							{
-								loadTestList += String(data['Items'][i]['sectionID']['N'])+"<br/>";
+					try {
+						db.scan(params, function(err, data) {
+							var loadTestList = "<p>"+rand+"<br/>";
+							if (data != null) {
+								for (var i = 0; i < data['Items'].length; i++) {
+									loadTestList += String(data['Items'][i]['sectionID']['N'])+"<br/>";
+								}
+								loadTestList += "</p>";
+								var headers = {};
+								headers["Content-Type"] = "text/html";
+  								headers["Access-Control-Allow-Origin"] = "*";
+								response.writeHead(200, headers);
+								response.write(loadTestList);
+								response.end();
 							}
-							loadTestList += "</p>";
-							var headers = {};
-							headers["Content-Type"] = "text/html";
-  							headers["Access-Control-Allow-Origin"] = "*";
-							response.writeHead(200, headers);
-							response.write(loadTestList);
-							response.end();}
-					});
+						});
 					}
-					catch(err)
-					{}
-					break;
-				case '3':
-					var uri = "mongodb://root:password@ds033699.mongolab.com:33699/ke_db",
-						db = mongojs.connect(uri, ["loadTest"]),
-						rand = Math.round(Math.random()*10000);
-					db.loadTest.find(function(err, docs) {
-    					var loadTestList = "<p>"+rand+"<br/>";
-						for(var i = 0; i < docs.length; i++)
-						{
-							loadTestList += String(docs[i]["Course"])+"<br/>";
-						}
-						loadTestList += "</p>";
-						var headers = {};
-						headers["Content-Type"] = "text/html";
-  						headers["Access-Control-Allow-Origin"] = "*";
-						response.writeHead(200, headers);
-						response.write(loadTestList);
-						response.end();
-					});
-					break;
-			}
+					catch(err) {}
+				}
+				catch(err) {}
+				break;
+			case '3':
+				var uri = "mongodb://root:password@ds033699.mongolab.com:33699/ke_db",
+					db = mongojs.connect(uri, ["loadTest"]),
+					rand = Math.round(Math.random()*10000);
+				db.loadTest.find(function(err, docs) {
+    				var loadTestList = "<p>"+rand+"<br/>";
+					for(var i = 0; i < docs.length; i++) {
+						loadTestList += String(docs[i]["Course"])+"<br/>";
+					}
+					loadTestList += "</p>";
+					var headers = {};
+					headers["Content-Type"] = "text/html";
+  					headers["Access-Control-Allow-Origin"] = "*";
+					response.writeHead(200, headers);
+					response.write(loadTestList);
+					response.end();
+				});
+				break;
 		}
+	}
 }
 
 exports.fileUpload = fileUpload;

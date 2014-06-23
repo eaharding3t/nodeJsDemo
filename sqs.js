5cba98b57dd22609e8aea8874f819c7faee849a6
kae2k <kae2k@virginia.edu>
2014-06-23 10:20:33 -0400

    test

--- sqs.js
+++ sqs.js
@@ -3,7 +3,7 @@ function sqsRequest(callback){
 	AWS.config.update({"accessKeyId": process.env.AWS_ACCESS_KEY_ID, "secretAccessKey": process.env.AWS_SECRET_KEY, "region": "us-east-1"});
 	var html = "";
 	var temp1 = "";
-	var temp2 = "";
+	var temp2;
 	var params = {
 		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/121717378798/poc-eh-queue'
 
@@ -12,7 +12,7 @@ function sqsRequest(callback){
 	sqs.receiveMessage(params, function(err, data){
 		//html += data['Messages'][0]['Body']['Subject'] + ' : ' + data['Messages'][0]['Body']['Message'];
 		temp1 += data['Messages'][0]['Body'];
-		temp2 = JSON.parse(temp);
+		temp2 = JSON.parse(temp1);
 		html = temp2['Subject'] + ' : ' + temp2['Message'];
 		callback(html);
 	});

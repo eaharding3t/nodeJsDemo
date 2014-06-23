f91acb7ed8b04c474ffbe30fc92a143c53b49d28
kae2k <kae2k@virginia.edu>
2014-06-23 09:47:50 -0400

    sqs

--- sqs.js
+++ sqs.js
@@ -10,19 +10,7 @@ function sqsRequest(callback){
 	var sqs = new AWS.SQS();
 	sqs.receiveMessage(params, function(err, data){
 		html += data['Messages'][0]['Body'];
-		params = {
-			QueueUrl: 'https://sqs.us-east-1.amazonaws.com/121717378798/poc-eh-queue',
-			MessageBody: 'testing'
-		};
-		sqs.sendMessage(params, function(err, data){
-				params = {
-				QueueUrl: 'https://sqs.us-east-1.amazonaws.com/121717378798/poc-eh-queue'
-			};
-			sqs.receiveMessage(params, function(err,data){
-				html += data['Messages'][0]['Body'];
-				callback(html);
-			});
-		});
+		callback(html);
 	});
 }
 exports.sqsRequest = sqsRequest;

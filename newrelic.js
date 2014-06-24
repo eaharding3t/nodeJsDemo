582034b5753021f6cf3fcaccc8d0cf9b2dc3fcd4
kae2k <kae2k@virginia.edu>
2014-06-24 10:33:18 -0400

    test

--- newrelic.js
+++ newrelic.js
@@ -4,8 +4,7 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-//var key = process.env.NEW_RELIC_LICENSE_KEY;
-//console.log(key);
+var key = process.env.NEW_RELIC_LICENSE_KEY;
 exports.config = {
   /**
    * Array of application names.
@@ -14,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : '27027ef2d0ea428aca5c128fcff94a603cf41516',
+  license_key : key,
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing

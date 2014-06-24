9c8cfc5c02ace2db4067b0a012f446dcb2159939
kae2k <kae2k@virginia.edu>
2014-06-24 09:46:54 -0400

    test

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : process.env.PARAM2,
+  license_key : String(process.env.NEW_RELIC_LICENSE_KEY),
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
